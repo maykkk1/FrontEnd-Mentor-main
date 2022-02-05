dbe8f1c231d4b1a4851dc7d08c2df766e0bb06e3
maykkk1 <maycon5591@gmail.com>
2022-02-05 20:46:43 -0300

    article-preview terminado

--- article-preview-component-master/script.js
+++ article-preview-component-master/script.js
@@ -7,7 +7,13 @@ function toggle () {
     const ShareSection = document.getElementById("share-section")
     if(ShareSection.classList.value === 'share-section') {
         ShareSection.classList.value = 'share-section-active'
+        btnShareSection.style.backgroundColor = 'hsl(214, 17%, 51%)'
+        btnShareSection.style.color = 'white'
+
+
     } else {
         ShareSection.classList.value = 'share-section'
+        btnShareSection.style.backgroundColor = 'hsl(210, 46%, 95%)'
+        btnShareSection.style.color = 'hsl(217, 19%, 35%)'
     }
 }
